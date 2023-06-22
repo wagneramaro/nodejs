63c46167bac07c18721ae60f4c09b9010bbf346a
Wagner Amaro <wagneramaro.dev@gmail.com>
2023-06-22 13:15:35 -0300

    Aula 7 - Entendendo import/export em CommonJS

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,9 @@
 let nome = 'Wagner';
 let idade = 36;
 console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
+const Matematica = require('./Matematica');
+let n1 = 10;
+let n2 = 25;
+console.log(`SOMA: ${Matematica.somar(n1, n2)}`);
+console.log(`SUBTRAÇÃO: ${Matematica.subtrair(n1, n2)}`);
+console.log(`MULTIPLICAÇÃO: ${Matematica.multiplicar(n1, n2)}`);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 let nome : string = 'Wagner';
 let idade :number = 36;
 console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
+
+const Matematica = require('./Matematica');
+
+let n1: number = 10;
+let n2: number = 25;
+
+
+
+console.log(`SOMA: ${Matematica.somar(n1, n2)}`);
+console.log(`SUBTRAÇÃO: ${Matematica.subtrair(n1, n2)}`);
+console.log(`MULTIPLICAÇÃO: ${Matematica.multiplicar(n1, n2)}`);
